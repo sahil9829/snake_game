0d4be9dee9f8a687aacdadc33535fc082997ecdc
Sahil Joshi <77918548+sahil9829@users.noreply.github.com>
2022-09-26 20:17:48 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-
+#Constants initialized
 STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -13,18 +13,20 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-
+        
+#To initialize the snake model to a start position, i.e centre of the screen
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
-
+            
+#Attributes of the snake segment that is added to the snake body
     def add_segment(self, position):
         new_segment = Turtle(shape="square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
-
+#To reset the game after the game is over
     def reset(self):
         for seg in self.segments:
             seg.goto(1000,1000)
@@ -32,9 +34,11 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+#extends the snake's body using the add_segment function at the final position of the list of segments
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
+        
+#this moves each segment starting from the last segment to the x and y coordinates of the previous segment to create a moving effect 
     def move(self):
         for seg_num in range(len(self.segments)-1,0,-1):
             new_x = self.segments[seg_num-1].xcor()
@@ -42,6 +46,7 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+#setting the function of the arrow keys to move the snake in the corresponding direction
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
